4e5c2adc56582b19cef312ede9cb342989d84a0e
adiintify <adityanama97@gmail.com>
2022-10-03 11:46:03 +0530

    Code Clean-up

--- main.py
+++ main.py
@@ -8,27 +8,27 @@ from requests.structures import CaseInsensitiveDict
 # Initialize parser
 parser = argparse.ArgumentParser()
 # Adding optional argument
-parser.add_argument("-f", "--file_path", help = "File path of the data input")
+parser.add_argument("-f", "--file_path", help="File path of the data input")
 
 # Adding optional argument
-parser.add_argument("-u", "--base_url", help = "Base url")
+parser.add_argument("-u", "--base_url", help="Base url")
 
 # Read arguments from command line
 args = parser.parse_args()
-base_url=args.base_url
-#URL to initiate scan on BurpSuite-------------------------------------
+base_url = args.base_url
+# URL to initiate scan on BurpSuite-------------------------------------
 
 headers = CaseInsensitiveDict()
 
-with open (args.file_path,"r") as f:
+with open(args.file_path, "r") as f:
     data = json.dumps(json.load(f))
 resp = requests.post(base_url, headers=headers, data=data)
-print("Status Code:-",resp.status_code)
-print("location of the scan result",resp.headers['location'])
+print("Status Code:-", resp.status_code)
+print("location of the scan result", resp.headers['location'])
 
-#Getting Data from JSON Body--------------------------------------------
+# Getting Data from JSON Body--------------------------------------------
 check_status = True
-location= resp.headers["location"]
+location = resp.headers["location"]
 check_url = base_url+"/"+location
 while check_status:
     resp = requests.get(check_url)
@@ -36,7 +36,7 @@ while check_status:
     print(respJson)
     scanstatus = respJson["scan_status"]
     print(scanstatus)
-    if scanstatus in ["succeeded","failed"]  :
+    if scanstatus in ["succeeded", "failed"]:
         check_url = False
         break
     else:
@@ -44,5 +44,6 @@ while check_status:
 issue_events = respJson['issue_events']
 print("no of issue: "+str(len(issue_events)))
 for issue in issue_events:
-    print("Vulnerability Name:"+issue['issue']['name']+",""Severity:"+issue['issue']['severity']+",""Origin:"+issue['issue']['origin']+",""Path:"+issue['issue']['path']+",""Confidence:"+issue['issue']['confidence'])
-print(json.dumps(respJson, indent = 1))
+    print("Vulnerability Name:"+issue['issue']['name']+",""Severity:"+issue['issue']['severity']+",""Origin:" +
+          issue['issue']['origin']+",""Path:"+issue['issue']['path']+",""Confidence:"+issue['issue']['confidence'])
+print(json.dumps(respJson, indent=1))
--- readme.md
+++ readme.md
@@ -1,18 +1,22 @@
 # BurpSuite API
-This is a script used to interact with the Burp Rest API. You must enable the Burp API for these scripts to work. To enable the API, go to User Options -> Misc -> REST API. The default address is 127.0.0.1:1337, and the scripts will default to that address. You can change the location with commandline options. You can choose to require an API key; the scripts will work either way. 
+
+This is a script used to interact with the Burp Rest API. You must enable the Burp API for these scripts to work. To enable the API, go to User Options -> Misc -> REST API. The default address is 127.0.0.1:1337, and the scripts will default to that address. You can change the location with commandline options. You can choose to require an API key; the scripts will work either way.
 Uses the Burp API to do an active scan on a single host, a file listing hosts, or a range of hosts.
 
 Usage
-Works with Python 3. This script Requires the Requests module. By default, the Burp API host is set to 127.0.0.1:1337 and the API is set to ''. If you specify a range, the tool will generate an IP range and run each address through a function that creates multiple URLs that attempts connections on multiple web ports. When providing URLs, the format should be http(s)://addr:port. If you don't provide URLs like this, that's okay, the tool will transform your URL to match that format. 
+Works with Python 3. This script Requires the Requests module. By default, the Burp API host is set to 127.0.0.1:1337 and the API is set to ''. If you specify a range, the tool will generate an IP range and run each address through a function that creates multiple URLs that attempts connections on multiple web ports. When providing URLs, the format should be http(s)://addr:port. If you don't provide URLs like this, that's okay, the tool will transform your URL to match that format.
 
 In order to run this you woul require to install requirments:-
 
-------------
-pip install -r requirements.txt
-------------
+---
+
+## pip install -r requirements.txt
+
 Do an active scan
 python3 main.py -u url of the burpsuite api -f filepath of the data.json
------------- 
---------
-python3 main.py -u http://127.0.0.1:1337/v0.1/scan -f /Users/mafiaguy/Desktop/data.json
---------
+
+---
+
+---
+
+## python3 main.py -u http://127.0.0.1:1337/v0.1/scan -f /Users/mafiaguy/Desktop/data.json
